c9c8f0d1ce9b7722812b8a9b67b0f59ba868b6fc
opeolipe <carolineratuolivia@gmail.com>
2026-05-20 22:20:24 +0800

    feat: Set base path and use relative links
    Configure Astro to use a base path of "/carol" for the site. Update all internal links and asset paths to use the `BASE_URL` environment variable to ensure correct routing when deployed under a sub-path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://carolineratuolivia.com",
+  base: "/carol",
   integrations: [tailwind()],
   output: "static",
   // Ensure the build doesn't inject inline styles for strict CSP
