0bc82a092967e0f4637952d44389ed04ce3a2887
opeolipe <carolineratuolivia@gmail.com>
2026-05-21 09:53:33 +0800

    feat: Configure astro base path and CSP
    Set the Astro application base path to "/carol" to enable deployment on a subdirectory.
    Add a Content-Security-Policy meta tag to the BaseLayout for enhanced security.
    Update dependencies, including adding @astrojs/check and typescript.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://carolineratuolivia.com",
+  base: "/carol",
   integrations: [tailwind(), react()],
   output: "static",
 });
